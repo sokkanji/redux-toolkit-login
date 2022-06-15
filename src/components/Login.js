cea87816f13ee8ea214ca883e8292f12bc6a42f3
Minji So <sskkanji@gmail.com>
2022-06-15 18:35:13 +0900

    Implement login function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/user";
 
 function Login() {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(login({ name: "sokkanji", age: 20, email: "react@gmail.com" }));
+  };
+
   return (
     <div>
-      <button>Login</button>
+      <button onClick={handleClick}>Login</button>
     </div>
   );
 }
--- src/redux/user.js
+++ src/redux/user.js
@@ -16,4 +16,6 @@ export const userSlice = createSlice({
   },
 });
 
+export const { login } = userSlice.actions;
+
 export default userSlice.reducer;
