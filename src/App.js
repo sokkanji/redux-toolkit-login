37a7ef04ff56ce3f12c0b8406dcf4c81a9c457f2
Minji So <sskkanji@gmail.com>
2022-06-15 18:10:44 +0900

    Add login, progile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile />
+      <Login />
+    </div>
+  );
 }
 
 export default App;
